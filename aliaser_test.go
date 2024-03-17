51b4035bdf8db71853c29cd2206c576579296574
Marco Zaccaro <marco@marcozaccaro.com>
2024-03-17 15:05:12 +0100

    test: improve test helpers

--- aliaser_test.go
+++ aliaser_test.go
@@ -21,46 +21,36 @@ func TestAliaserOptions(t *testing.T) {
 		_, err := New(TestTarget, TestPattern, WithContext(ctx))
 		assert.Error(t, err)
 	})
-	t.Run("ExcludeConstants", func(t *testing.T) {
-		AliaserTest(t, func(t *testing.T, a *Aliaser) {
-			assert.Empty(t, a.alias.Constants)
-		}, ExcludeConstants())
-	})
-	t.Run("ExcludeVariables", func(t *testing.T) {
-		AliaserTest(t, func(t *testing.T, a *Aliaser) {
-			assert.Empty(t, a.alias.Variables)
-		}, ExcludeVariables())
-	})
-	t.Run("ExcludeFunctions", func(t *testing.T) {
-		AliaserTest(t, func(t *testing.T, a *Aliaser) {
-			assert.Empty(t, a.alias.Functions)
-		}, ExcludeFunctions())
-	})
-	t.Run("ExcludeTypes", func(t *testing.T) {
-		AliaserTest(t, func(t *testing.T, a *Aliaser) {
-			assert.Empty(t, a.alias.Types)
-		}, ExcludeTypes())
-	})
-	t.Run("ExcludeNames", func(t *testing.T) {
-		AliaserTest(t, func(t *testing.T, a *Aliaser) {
-			ObjectBatchTest(t, a.alias.Constants, func(t *testing.T, o types.Object) {
-				assert.NotEqual(t, "A", o.Name())
-				assert.NotEqual(t, "D", o.Name())
-			})
-			ObjectBatchTest(t, a.alias.Variables, func(t *testing.T, o types.Object) {
-				assert.NotEqual(t, "A", o.Name())
-				assert.NotEqual(t, "D", o.Name())
-			})
-			ObjectBatchTest(t, a.alias.Functions, func(t *testing.T, o types.Object) {
-				assert.NotEqual(t, "A", o.Name())
-				assert.NotEqual(t, "D", o.Name())
-			})
-			ObjectBatchTest(t, a.alias.Types, func(t *testing.T, o types.Object) {
-				assert.NotEqual(t, "A", o.Name())
-				assert.NotEqual(t, "D", o.Name())
-			})
-		}, ExcludeNames("A", "D"))
-	})
+	t.Run("ExcludeConstants", AliaserTest(func(t *testing.T, a *Aliaser) {
+		assert.Empty(t, a.alias.Constants)
+	}, ExcludeConstants()))
+	t.Run("ExcludeVariables", AliaserTest(func(t *testing.T, a *Aliaser) {
+		assert.Empty(t, a.alias.Variables)
+	}, ExcludeVariables()))
+	t.Run("ExcludeFunctions", AliaserTest(func(t *testing.T, a *Aliaser) {
+		assert.Empty(t, a.alias.Functions)
+	}, ExcludeFunctions()))
+	t.Run("ExcludeTypes", AliaserTest(func(t *testing.T, a *Aliaser) {
+		assert.Empty(t, a.alias.Types)
+	}, ExcludeTypes()))
+	t.Run("ExcludeNames", AliaserTest(func(t *testing.T, a *Aliaser) {
+		ObjectBatchHelper(t, a.alias.Constants, func(t *testing.T, o types.Object) {
+			assert.NotEqual(t, "A", o.Name())
+			assert.NotEqual(t, "D", o.Name())
+		})
+		ObjectBatchHelper(t, a.alias.Variables, func(t *testing.T, o types.Object) {
+			assert.NotEqual(t, "A", o.Name())
+			assert.NotEqual(t, "D", o.Name())
+		})
+		ObjectBatchHelper(t, a.alias.Functions, func(t *testing.T, o types.Object) {
+			assert.NotEqual(t, "A", o.Name())
+			assert.NotEqual(t, "D", o.Name())
+		})
+		ObjectBatchHelper(t, a.alias.Types, func(t *testing.T, o types.Object) {
+			assert.NotEqual(t, "A", o.Name())
+			assert.NotEqual(t, "D", o.Name())
+		})
+	}, ExcludeNames("A", "D")))
 }
 
 func TestAliaserError(t *testing.T) {
@@ -88,67 +78,61 @@ func TestAliaserError(t *testing.T) {
 		a := &Aliaser{}
 		assert.Error(t, a.Generate(io.Discard)) // empty a.alias
 	})
-	t.Run("Format", func(t *testing.T) {
-		AliaserTest(t, func(t *testing.T, a *Aliaser) {
-			c0 := a.alias.Constants[0] // change the first constant to have an invalid name
-			a.alias.Constants[0] = types.NewConst(c0.Pos(), c0.Pkg(), c0.Name()+".", c0.Type(), c0.Val())
-			assert.Error(t, a.Generate(io.Discard))
-		})
-	})
-	t.Run("Write", func(t *testing.T) {
-		AliaserTest(t, func(t *testing.T, a *Aliaser) {
-			assert.ErrorIs(t, a.Generate(WriterE{}), assert.AnError)
-		})
-	})
-	t.Run("File", func(t *testing.T) {
+	t.Run("Format", AliaserTest(func(t *testing.T, a *Aliaser) {
+		c0 := a.alias.Constants[0] // change the first constant to have an invalid name
+		a.alias.Constants[0] = types.NewConst(c0.Pos(), c0.Pkg(), c0.Name()+".", c0.Type(), c0.Val())
+		assert.Error(t, a.Generate(io.Discard))
+	}))
+	t.Run("Write", AliaserTest(func(t *testing.T, a *Aliaser) {
+		assert.ErrorIs(t, a.Generate(WriterE{}), assert.AnError)
+	}))
+	t.Run("File", AliaserTest(func(t *testing.T, a *Aliaser) {
 		tempDir := t.TempDir()
 		nwDir := filepath.Join(tempDir, "non-writable-dir")
 		require.NoError(t, os.Mkdir(nwDir, 0o555))
-		AliaserTest(t, func(t *testing.T, a *Aliaser) {
-			t.Run("Mkdir", func(t *testing.T) {
-				assert.Error(t, a.GenerateFile(filepath.Join(nwDir, "out/alias.go")))
-			})
-			t.Run("OpenFile", func(t *testing.T) {
-				assert.Error(t, a.GenerateFile(filepath.Join(nwDir, "alias.go")))
-			})
+		t.Run("Mkdir", func(t *testing.T) {
+			assert.Error(t, a.GenerateFile(filepath.Join(nwDir, "out/alias.go")))
 		})
-	})
+		t.Run("OpenFile", func(t *testing.T) {
+			assert.Error(t, a.GenerateFile(filepath.Join(nwDir, "alias.go")))
+		})
+	}))
 }
 
 func TestSetAlias(t *testing.T) {
-	t.Run("NotExportedObject", func(t *testing.T) {
-		AliaserTest(t, func(t *testing.T, a *Aliaser) {
-			LoadedPackageTest(t, func(t *testing.T, p *packages.Package) {
-				p.Types.Scope().Insert(
-					types.NewConst(0, p.Types, "notExported", types.Typ[types.Uint8], nil),
-				)
-				assert.NoError(t, a.setAlias(a.alias.Config, p)) // cover not exported object
-			})
+	t.Run("NotExportedObject", AliaserTest(func(t *testing.T, a *Aliaser) {
+		LoadedPackageHelper(t, func(t *testing.T, p *packages.Package) {
+			p.Types.Scope().Insert(
+				types.NewConst(0, p.Types, "notExported", types.Typ[types.Uint8], nil),
+			)
+			assert.NoError(t, a.setAlias(a.alias.Config, p)) // cover not exported object
 		})
-	})
-	t.Run("ObjectTypeError", func(t *testing.T) {
-		AliaserTest(t, func(t *testing.T, a *Aliaser) {
-			LoadedPackageTest(t, func(t *testing.T, p *packages.Package) {
-				p.Types.Scope().Insert(
-					types.NewLabel(0, p.Types, "MyLabel"),
-				)
-				assert.Error(t, a.setAlias(a.alias.Config, p))
-			})
+	}))
+	t.Run("ObjectTypeError", AliaserTest(func(t *testing.T, a *Aliaser) {
+		LoadedPackageHelper(t, func(t *testing.T, p *packages.Package) {
+			p.Types.Scope().Insert(
+				types.NewLabel(0, p.Types, "MyLabel"),
+			)
+			assert.Error(t, a.setAlias(a.alias.Config, p))
 		})
-	})
+	}))
 }
 
-// AliaserTest is a helper function that creates a new valid Aliaser with
-// [TestTarget], [TestPattern] and the options, then, it calls the given function.
-func AliaserTest(t *testing.T, fn func(*testing.T, *Aliaser), opts ...Option) {
-	t.Helper()
-	a, err := New(TestTarget, TestPattern, opts...)
-	assert.NoError(t, err)
-	require.NotNil(t, a.alias)
-	fn(t, a)
+// AliaserTest returns a subtest function, compatible with the [testing.T.Run]
+// method, that creates a new valid [Aliaser] with [TestTarget], [TestPattern]
+// and the options, then, it calls the given function.
+func AliaserTest(fn func(*testing.T, *Aliaser), opts ...Option) func(t *testing.T) {
+	return func(t *testing.T) {
+		a, err := New(TestTarget, TestPattern, opts...)
+		assert.NoError(t, err)
+		require.NotNil(t, a.alias)
+		fn(t, a)
+	}
 }
 
-func LoadedPackageTest(t *testing.T, fn func(*testing.T, *packages.Package)) {
+// LoadedPackageHelper is a helper function that load a valid [packages.Package]
+// with [TestPattern] and calls the given function with t and the package.
+func LoadedPackageHelper(t *testing.T, fn func(*testing.T, *packages.Package)) {
 	t.Helper()
 	pkgs, err := packages.Load(&packages.Config{Mode: loadMode}, TestPattern)
 	require.NoError(t, err)
@@ -157,7 +141,9 @@ func LoadedPackageTest(t *testing.T, fn func(*testing.T, *packages.Package)) {
 	fn(t, p)
 }
 
-func ObjectBatchTest[TO types.Object](t *testing.T, objs []TO, fn func(*testing.T, types.Object)) {
+// ObjectBatchHelper is a helper function that iterates over the given objects
+// and calls the given function with t and the object.
+func ObjectBatchHelper[TO types.Object](t *testing.T, objs []TO, fn func(*testing.T, types.Object)) {
 	t.Helper()
 	for _, obj := range objs {
 		fn(t, obj)
